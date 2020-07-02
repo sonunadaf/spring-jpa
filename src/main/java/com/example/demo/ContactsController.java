ea6f5ac79e94f1b335e76c0ccb6249a4759c6c4d
sonunadaf <msd.nadaf@gmail.com>
2020-07-02 16:53:50 +0530

    iterating list of contacts in jsp

--- src/main/java/com/example/demo/ContactsController.java
+++ src/main/java/com/example/demo/ContactsController.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,17 +15,22 @@ public class ContactsController {
 	@Autowired
 	ContactRepository repository;
 
+	@GetMapping("/contacts")
+	public String getContacts(Model model) {
+		Iterable<Contact> contacts = repository.findAll();
+		model.addAttribute("contactList", contacts);
+		return "contact";
+	}
+
 	@PostMapping("/save")
-	public String insert(@RequestBody Contact contact) {
+	public void insert(@RequestBody Contact contact) {
 		repository.save(contact);
-		return "created";
 
 	}
 
 	@DeleteMapping("/delete/{id}")
-	public String insert(@PathVariable int id) {
+	public void insert(@PathVariable int id) {
 		repository.deleteById(id);
-		return "deleted";
 
 	}
 
--- src/main/webapp/WEB-INF/jsp/contact.jsp
+++ src/main/webapp/WEB-INF/jsp/contact.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ page import="java.util.List"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,11 @@
 <title>Insert title here</title>
 </head>
 <body>
-	
+	<c:forEach items="${contactList}" var="contacts">
+		<tr>
+			<td>Employee ID: <c:out value="${contacts.id}" /></td>
+			<td>Employee Pass: <c:out value="${contacts.name}" /></td>
+		</tr>
+	</c:forEach>
 </body>
 </html>
