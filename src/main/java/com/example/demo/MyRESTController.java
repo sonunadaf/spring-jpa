e8f8b3d5b766a91ae51bad4d8aa1d32bbf215fe3
sonunadaf <msd.nadaf@gmail.com>
2020-07-02 16:43:22 +0530

    created controlelr

--- src/main/java/com/example/demo/MyRESTController.java
+++ src/main/java/com/example/demo/MyRESTController.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +19,20 @@ public class MyRESTController {
 	@GetMapping("/contacts")
 	public Iterable<Contact> getContacts() {
 		return repository.findAll();
+	}
+
+	@PostMapping("/save")
+	public ResponseEntity<String> insert(@RequestBody Contact contact) {
+		repository.save(contact);
+		return new ResponseEntity<String>("Created", HttpStatus.OK);
+
+	}
+
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> insert(@PathVariable int id) {
+		repository.deleteById(id);
+		return new ResponseEntity<String>("Deleted", HttpStatus.OK);
+
 	}
 
 }
